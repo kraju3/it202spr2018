6a07012bdf0fa30b69246c322c5926bdb7fcfd6a
Kiran Raju <kraju3@uic.edu>
2018-04-20 05:18:24 +0000

    Service Worker

---
+++ bigproject/sw.js
@@ -0,0 +1,15 @@
+var cacheName = 'findeventscache';
+var filesToCache = [
+  './',
+  './index.html'
+  ];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+})
