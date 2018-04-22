d8b970179e8947e03427bfbe139f76f255321229
Kiran Raju <kraju3@uic.edu>
2018-04-22 22:25:16 +0000

    big project commit 10

--- bigproject/sw.js
+++ bigproject/sw.js
@@ -13,3 +13,32 @@ self.addEventListener('install', function(e) {
     })
   );
 })
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+
+
+
+
